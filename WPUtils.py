40e18dc7d6b59a6049915085100c0aca66bf780c
bello_ishaga <belloishaga@outlook.com>
2024-02-04 14:15:23 +0100

    script working properly, recovering old urls and creating a redirect fr them

---
+++ WPUtils.py
@@ -0,0 +1,122 @@
+
+import urllib.parse
+import re
+import random
+import requests
+import time
+import base64
+import json
+from urllib import parse as urlparse
+from PIL import Image, ImageColor, ImageEnhance
+from io import BytesIO
+from secrets import token_hex
+from datetime import datetime, timedelta
+
+session = requests.Session()
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+}
+session.headers.update(headers)
+
+def header(user, password):
+  credentials = user + ':' + password
+  token = base64.b64encode(credentials.encode())
+  header_json = {'Authorization': 'Basic ' + token.decode('utf-8')}
+  return header_json
+
+def get_cat_id(category, logs):
+  hed = header(logs['wpUN'],logs['wpPW'])
+  data = {'name': category}
+
+  responce = session.post(f"http://{logs['wpDomain']}/wp-json/wp/v2/categories", headers = hed, json = data)
+
+  if responce.status_code == 201:
+    category_id = (responce.json()['id'])
+  elif responce.status_code == 400:
+    category_id = (responce.json()['data']['term_id'])
+  else:
+    print("There was an error with the category")
+
+  return category_id
+
+def get_img_id(url, logs):
+  def imgPost(data):
+    title = f'pexels-photo-15792909'
+    headers = {"Content-Type": "image/jpeg", "Accept": "application/json", 'Content-Disposition': f"attachment; filename={title}-{token_hex(20)}.jpg",}
+    auth = (logs['wpUN'], logs['wpPW'])
+    return session.post(url = f'http://{logs["wpDomain"]}/wp-json/wp/v2/media', auth=auth, headers=headers, data=data).json()
+
+  data = requests.get(url).content
+  image = Image.open(BytesIO(data))
+  enhancer = ImageEnhance.Color(image)
+  fI = enhancer.enhance(random.uniform(.5,.5))
+  imgData = BytesIO()
+  fI.save(imgData, format='JPEG')
+  img_data_value = imgData.getvalue()
+
+  return imgPost(img_data_value)['id']
+
+
+def upload_content(post_data, logs):
+  hed = header(logs['wpUN'],logs['wpPW'])
+  responce = session.post(f"http://{logs['wpDomain']}/wp-json/wp/v2/posts", headers = hed, json = post_data)
+  return responce
+
+# function to control redirection plugin
+def redirect(from_url, to_url, logs):
+  # Base URL for the Redirection plugin's REST API
+  base_url = f"https://{logs['wpDomain']}/wp-json/redirection/v1/"
+
+  # Authentication credentials (replace with your actual credentials)
+  username = logs['wpUN']
+  password = logs['wpPW']  # Or use an API key if available
+
+  # Data for the redirect you want to create
+  json_data = {
+      'url': f'{from_url}',
+      'title': '',
+      'match_data': {
+          'source': {
+              'flag_regex': False,
+              'flag_trailing': True,
+              'flag_case': True,
+              'flag_query': 'exact',
+          },
+          'options': {},
+      },
+      'match_type': 'url',
+      'action_type': 'url',
+      'position': 0,
+      'group_id': 1,
+      'action_code': 301,
+      'action_data': {
+          'url': f'{to_url}',
+      },
+  }
+
+  # Make a POST request to create the redirect
+  response = requests.post(
+      base_url + "redirect",
+      json=json_data,
+      auth=(username, password)
+  )
+
+  return response.text
+
+if __name__ == "__main__":
+  data = {
+      'title' : 'test',
+      'content' : 'test content',
+      'status' : 'draft',
+  }
+
+  logs = {
+    'wpUN': 'groupetravail237',
+    'wpPW': 'AfjR VVnj GlTz z6qp s2cy R6I7',
+    'wpDomain': 'wayback-resurection.000webhostapp.com',
+  }
+
+  base_url = "/2020/11/20/des-opportunites-plus-frequentes-quon-ne-penserait/"
+  dest_url = "https://wayback-resurection.000webhostapp.com/this-is-the-title-here/"
+
+  print(redirect(base_url, dest_url, logs))
---
+++ wayback_script.py
@@ -0,0 +1,146 @@
+import wayback
+import re
+from wayback import WaybackClient
+from bs4 import BeautifulSoup
+import requests
+import urllib
+from lxml.html.clean import Cleaner
+import pandas as pd
+import WPUtils as wp
+
+# this function is used to clean the html restored
+def sanitize(dirty_html):
+  cleaner = Cleaner(page_structure=True,
+                meta=True,
+                embedded=True,
+                links=True,
+                style=True,
+                processing_instructions=True,
+                inline_style=True,
+                scripts=True,
+                javascript=True,
+                comments=True,
+                frames=True,
+                forms=True,
+                annoying_tags=True,
+                remove_unknown_tags=True,
+                safe_attrs_only=True,
+                safe_attrs=frozenset(['src','color', 'href', 'title', 'class', 'name', 'id']),
+                remove_tags=('span', 'font', 'div')
+                )
+
+  return cleaner.clean_html(dirty_html)
+
+# this function is used to list all urls that are available and returns a 200
+def available_archived_urls(site):
+  headers = {
+      'authority': 'web.archive.org',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.9',
+      'dnt': '1',
+      'referer': 'https://web.archive.org/web/sitemap/djangutech.com',
+      'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+  }
+
+  response = requests.get(
+      f'https://web.archive.org/web/timemap/json?url={site}&fl=timestamp:4,original,urlkey&matchType=prefix&filter=statuscode:200&filter=mimetype:text/html&collapse=urlkey&collapse=timestamp:4&limit=100000',
+      headers=headers,
+  )
+
+  return  response.json()
+
+# this function is used to retrieve an archived url in a possibly html format
+def get_archived_content(url):
+  
+  client = WaybackClient()
+  results = client.search(url)
+  record = next(results)
+  archived_url = record.view_url
+  page = requests.get(archived_url)
+
+  soup = BeautifulSoup(page.text, 'lxml')
+
+  # find the title of the page
+  title = soup.find('title').text
+
+  # Find the following img tag after the h1 tag
+  h1_tag = soup.find('h1')
+  first_img_after_h1 = h1_tag.find_next('img')
+  image_url = first_img_after_h1.get('src')
+
+# clean the image url
+  cleaned_url = urllib.parse.urlparse(image_url)
+  cleaned_url = urllib.parse.ParseResult(scheme=cleaned_url.scheme, netloc=cleaned_url.netloc, path=cleaned_url.path, params='', query='', fragment='')
+  clean_image_url = urllib.parse.urlunparse(cleaned_url)
+
+  # Create a new HTML document with the extracted elements
+  relevant_elements = soup.find_all(['h1', 'h2', 'h3', 'p'])
+  cleaned_html = "<html><head></head><body>"
+  cleaned_html += "".join(str(element) for element in relevant_elements)
+  cleaned_html += "</body></html>"
+  cleaned_html = sanitize(cleaned_html)
+
+  # return the title, the cleaned html and the image url in dictionary format
+  return {'title': title, 'content': cleaned_html, 'image_url': clean_image_url}
+
+# function to clean urls
+def clean_url(url):
+  pattern = r'(https?://[^\/]+)'
+  cleaned_url = re.sub(pattern, '', url)
+  return cleaned_url
+
+def recover(url):
+  # get the content of the first url
+  content = get_archived_content(url)
+
+  # get the category id
+  category_id = wp.get_cat_id('Uncategorized', logs)
+
+  # get the image id
+  try:
+    image_id = wp.get_img_id(content['image_url'], logs)
+  except:
+    image_id = None
+    pass
+
+  # create the post data
+  post_data = {
+      'title': content['title'],
+      'content': content['content'],
+      'status': 'publish',
+      'categories': [category_id],
+      'featured_media': image_id
+  }
+
+  # upload the content
+  upload = wp.upload_content(post_data, logs)
+
+  if upload.status_code == 201:
+    print("Recovered content has been uploaded successfully")
+  else:
+    print(upload.status_code)
+  
+
+  print("Trying to redirect the old url to the new one")
+  url_to = upload.json()['guid']['rendered']
+  url_to = clean_url(url_to)
+  url_from = clean_url(url)
+  print(wp.redirect(url_from, url_to, logs))
+
+if __name__ == '__main__':
+
+  logs = {
+      'wpUN': 'groupetravail237',
+      'wpPW': 'AfjR VVnj GlTz z6qp s2cy R6I7',
+      'wpDomain': 'wayback-resurection.000webhostapp.com',
+  }
+  
+  url = "http://www.rvlf.fr/telltale-behind-the-scenes-1-et-2-du-jeu-bttf-rvlf-★-retour-vers-le-futur-bttf-★-back-to-the-future/Jeu-retour-vers-le-futur-2010"
+
+  recover(url)
